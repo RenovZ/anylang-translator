96b8e773f6d8bb4851893432c350efb9193d0356
RenovZ <cgsixv@gmail.com>
2026-03-14 19:47:01 +0800

    The first commit

---
+++ src/entrypoints/popup/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import svelteLogo from "../../assets/svelte.svg";
+    import Counter from "../../lib/Counter.svelte";
+    import "./app.css";
+</script>
+
+<main>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</main>
+
+<style>
+</style>
