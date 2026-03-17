100e5be0acbb98f6c0fb516aa1923399d06b93ef
RenovZ <cgsixv@gmail.com>
2026-03-17 11:06:35 +0800

    Clean up popup scaffold

--- src/lib/avatar.ts
+++ src/lib/avatar.ts
@@ -1,8 +1,6 @@
 import { createAvatar } from '@dicebear/core';
 import { initials } from '@dicebear/collection';
 
-import { hashCode } from './hash';
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const dicebear = (username: string | undefined, options = {}, style: any = undefined) => {
   const defaultOptions = { seed: username };
